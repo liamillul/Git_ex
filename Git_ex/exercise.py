6e6a6bcb3cde77c0ff6a496daaeb4cc8e0ae7b65
lia <liamillul@gmail.com>
2023-09-13 15:52:52 +0300

    initial commit

--- Git_ex/exercise.py
+++ Git_ex/exercise.py
@@ -1,11 +1,9 @@
 #1
-print(" Hello Enter your  name: ")
+print(" Hello Enter your  name: Lia")
 
 #2
-print(" Hello Enter your adress: ")
+print(" Hello Enter your adress: Avigdor, Hahasidim 61")
 
-
-"""
 name = "Name" 
 print(" Hello " , name ," How are you: " )
-"""
+
