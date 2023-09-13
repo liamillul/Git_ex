c4114e4e2f611147fcb155996297872b38933ae7
noga <noga.hosi@gmail.com>
2023-09-13 15:59:04 +0300

    commit

--- Git_ex/exercise.py
+++ Git_ex/exercise.py
@@ -2,7 +2,7 @@
 print(" Hello Enter your  name: Lia")
 
 #2
-print(" Hello Enter your adress: Avigdor, Hahasidim 61")
+print("elephant")
 
 name = "Name" 
 print(" Hello " , name ," How are you: " )
